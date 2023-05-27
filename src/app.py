e6cf312150dde8870242e097c3679ccecf639cd9
ikumyn1or0 <19ming3@gmail.com>
2023-05-27 14:01:03 +0900

    #81と#82に対応

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from pages import overview, episode, browse, about
 
 
 PAGE_TITLE = "裏ラジアーカイブ(仮)"
-LAST_UPDATE = "2023-04-29"
+LAST_UPDATE = "2023-05-27"
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
